0a580a8126482b034abaef4e5825b4a08a956df8
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-08-10 13:59:03 +0530

    Done

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,6 +12,13 @@ app.use(cors({
   origin: "https://insta-lite-ai-captions.vercel.app",
   credentials: true
 }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://insta-lite-ai-captions.vercel.app"/);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(express.json());
 app.use(cookieParser());
