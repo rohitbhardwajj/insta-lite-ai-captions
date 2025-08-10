8dc1e0572423d7f4d6db2c8d1729bd652b413768
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-08-10 15:40:50 +0530

    Done

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ const connectDB = require('./src/db/db')
 
 require('dotenv').config();
 // server running
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
 
 // db connect
 connectDB();
