f2a83cc8bfcac3a654afcfdb9170755604790c1b
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-08-10 13:51:30 +0530

    Done

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,17 +8,13 @@ const authRoutes = require('./routes/auth.routes');
 const postRoutes = require('./routes/posts.routes');
 
 const app = express();
-
-
-app.use(express.json());
-app.use(cookieParser());
 app.use(cors({
   origin: "https://insta-lite-ai-captions.vercel.app",
   credentials: true
 }));
 
-
-
+app.use(express.json());
+app.use(cookieParser());
 
 
 
